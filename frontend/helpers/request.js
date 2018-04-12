7990e753afab8b260088d6762144d38da53639f7
TarikBauerVentura <tarik.bauer.ventura@gmail.com>
2018-04-12 02:16:07 -0300

    deploy version

--- frontend/helpers/request.js
+++ frontend/helpers/request.js
@@ -2,11 +2,12 @@ export default class Request {
 
     post(path, body) {
         return new Promise((resolve, reject) => {
-            fetch('http://lab-project.us-east-1.elasticbeanstalk.com/api' + (path.startsWith('/') ? path : '/' + path), {
-                method: 'POST',
-                credentials: 'same-origin',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(body),
+            fetch('http://lab-project.us-east-1.elasticbeanstalk.com/api' + (path.startsWith('/') ? path : '/' + path),
+                {
+                    method: 'POST',
+                    credentials: 'same-origin',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(body),
             }).then(response => resolve(response.json())).catch(error => reject(error))
         })
     }
